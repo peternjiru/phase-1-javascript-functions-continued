530b3478f9a486954c8a2f5bd490e3fd87c06aa7
Peter Njiru <peternjirum@gmail.com>
2023-03-18 18:21:44 -0400

    completed java functions continued lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+//console.log(saturdayFun());
+
+const mondayWork = function (task = "go to the office") {
+    return `This Monday, I will ${task}.`;
+}
+
+// console.log(mondayWork());
+
+function wrapAdjective(flair = "*") {
+    return function (work = "special") {
+        return `You are ${flair}${work}${flair}!`;
+    }
+}
+
+const encouragingPromptFunction = wrapAdjective("%")("a dedicated programmer");
+console.log(encouragingPromptFunction);
